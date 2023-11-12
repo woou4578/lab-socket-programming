4f6513780379dae4e72d66f7a8f66e5fa61c56bf
SeoJeongchan <wjdcks822@gmail.com>
2023-11-12 18:00:11 +0900

    change 2

--- tcp-server.py
+++ tcp-server.py
@@ -15,7 +15,7 @@ while 1:
     print('Client address:', addr)
     data = conn.recv(BUFFER_SIZE)
     #if not data: break
-    currentTime = " " + " first updated !!! " + time.ctime(time.time()) + "\r\n"
+    currentTime = " " + " restore updated !!! " + time.ctime(time.time()) + "\r\n"
     print(data.decode('utf-8'))
     data = data + currentTime.encode('ascii')
     conn.send(data)
